ab844cec7ecefd8e3659730b98a5abe6bd13029e
venkatesh6storage <venkatesh@6storage.com>
2021-08-30 00:02:48 +0530

    hold button function implemented

--- script.js
+++ script.js
@@ -22,9 +22,22 @@ diceEl.classList.add('hidden');
 const scores = [0,0];
 let currentScore = 0;
 let activePlayer = 0;
+let playing = true;
+
+const switchPlayer = function(){
+
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer == 0 ? 1 :0;
+    player0El.classList.toggle(`player--active`);
+    player1El.classList.toggle(`player--active`);
+   
+}
 //Rolling dice functionality
 
-btnRoll.addEventListener('click',function(){
+btnRoll.addEventListener('click',function()
+{
+    if(playing){
 
 //Generating random dice roll
  const dice = Math.trunc(Math.random()*6)+1;
@@ -50,25 +63,53 @@ document.getElementById(`current--${activePlayer}`).textContent = currentScore;
 
 
 }else {
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    activePlayer = activePlayer == 0 ? 1 :0;
-    player0El.classList.toggle(`player--active`);
-    player1El.classList.toggle(`player--active`);
    
+switchPlayer();
 
-
-//switch to next player
+//switch to nex player
 
 }
 
-
-
+    }
 
 })
 
 
 
+btnHold.addEventListener('click',function(){
+    if(playing){
+
+ //add current score to active player scores
+   scores[activePlayer] += currentScore;
+   
+ document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
+
+ //check if player's score is >=100
+//finish the game
+
+if(scores[activePlayer] >= 20) {
+   playing = false;
+   diceEl.classList.add('hidden');
+    //finish the game
+    document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
+    document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');    
+
+}else {
+
+    //switch to the next player
+    switchPlayer();
+}
+
+
+
+
+
+//switch to next player
+
+switchPlayer();
+};
+
+})
 
 
 
