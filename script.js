3ea8ed810e000c40799c0f70207d6793b2de6ae1
venkatesh6storage <venkatesh@6storage.com>
2021-09-05 18:16:04 +0530

    new buuton implementation

--- script.js
+++ script.js
@@ -15,14 +15,30 @@ const btnHold = document.querySelector('.btn--hold');
 
 //starting condition
 
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+let scores,currentScore,activePlayer,playing;
 
-const scores = [0,0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+const init = function() {  
+   
+
+   scores = [0,0];
+    currentScore = 0;
+    activePlayer = 0;
+    playing = true;
+
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    current0El.textContent=0;
+    current1El.textContent=0;
+
+   diceEl.classList.add('hidden');  
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player1El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+  
+}
+
+init();
 
 const switchPlayer = function(){
 
@@ -104,6 +120,7 @@ if(scores[activePlayer] >= 20) {
 
 
 
+
 //switch to next player
 
 switchPlayer();
@@ -113,3 +130,4 @@ switchPlayer();
 
 
 
+btnNew.addEventListener('click',init)
